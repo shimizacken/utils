024f0a0a316b7b247ea6647166339a0f7133f368
Shimi Zacken <shimizacken@gmail.com>
2021-05-07 22:47:15 +0200

    promise supported

--- src/debounce.ts
+++ src/debounce.ts
@@ -1,11 +1,30 @@
-export const debounce = (fn, duration: number) => {
-  let timeoutId: number;
+type Fn<T extends unknown[], R> = (...args: T) => R;
 
-  return (...args) => {
+export const debounce = <T extends unknown[], R>(
+  fn: Fn<T, R>,
+  duration: number,
+  log = false
+) => {
+  let timeoutId: number;
+  let callCounter = 0;
+
+  return function (this: void, ...args: T) {
+    if (log) {
+      console.log("fn called 🤙", ++callCounter);
+    }
+
     clearTimeout(timeoutId);
 
-    timeoutId = setTimeout(() => {
-      fn(...args);
-    }, duration);
+    return new Promise<R>((resolve) => {
+      timeoutId = window.setTimeout(() => {
+        const fnResult = fn.apply(this, args);
+        resolve(fnResult);
+
+        if (log) {
+          console.log("fn invoked 🚀");
+          console.log("fnResult 📦", fnResult);
+        }
+      }, duration);
+    });
   };
 };
